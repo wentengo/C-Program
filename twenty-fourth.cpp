e85d9c2314f9134c0bba282f2bc7b56cea3a7a03
wentengo <1910387599@qq.com>
2018-11-19 15:20:47 +0800

    计算两个int（32位）整形有多少个比特位不同

---
+++ twenty-fourth.cpp
@@ -0,0 +1,23 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include "stdio.h"
+
+void function(int *num)
+{
+	int i, j;
+	for (i = 1; i <= *num;i++)
+	{
+		for (j = 1; j <= i; j++)
+		{
+			printf("%d*%d=%d  ", j, i, i*j);
+		}
+		printf("\n");
+	}
+}
+
+int main()
+{
+	int n;
+	scanf("%d", &n);
+	function(&n);
+	return 0;
+}
