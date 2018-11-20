841c1a901df62881c7f5988a380af21d45a87dcd
wentengo <44473264+wentengo@users.noreply.github.com>
2018-11-20 08:34:24 +0800

    Update twenty-third.cpp

--- twenty-third.cpp
+++ twenty-third.cpp
@@ -1,26 +1,23 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include "stdio.h"
+
+void function(int *num)
+{
+	int i, j;
+	for (i = 1; i <= *num;i++)
+	{
+		for (j = 1; j <= i; j++)
+		{
+			printf("%d*%d=%d  ", j, i, i*j);
+		}
+		printf("\n");
+	}
+}
+
 int main()
 {
-	int i, x, y, count = 0;
-	int a[32] = { 0 };
-	int b[32] = { 0 };
-	scanf("%d.%d", &x, &y);
-	for (i = 31; i >= 0; i--)
-	{
-		a[i] = x % 2;
-		x /= 2;
-	}
-	for (i = 31; i >= 0; i--)
-	{
-		b[i] = y % 2;
-		y /= 2;
-	}
-	for (i = 0; i < 32; i++)
-	{
-		if (a[i] != b[i])
-			count++;
-	}
-	printf("%d\n", count);
+	int n;
+	scanf("%d", &n);
+	function(&n);
 	return 0;
 }
