9e60d50d784610058c7f04dc01fa2a4ff1fc4b22
wentengo <1910387599@qq.com>
2018-12-04 08:55:13 +0800

    写一个递归函数DigitSum(n)，输入一个非负整数，返回组成它的数字之和，例如，调用DigitSum(1729)，则应该返回1 + 7 + 2 + 9，它的和是19

--- thirty-seventh.cpp
+++ thirty-seventh.cpp
@@ -1,56 +1,18 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include "stdio.h"
-#include "assert.h"
 
-int JS(const char *a)
+int DigitSum(int n)
 {
-	int count = 0;
-	assert(a != NULL);
-	while (*a)
-	{
-		count++;
-		a++;
-	}
-	return count;
-}
-
-void NZ(char *left,char *right)
-{
-	assert(left != NULL);
-	assert(right!= NULL);
-	while (left < right)
-	{
-		char tmp = *left;
-		*left = *right;
-		*right = tmp;
-		left++;
-		right--;
-	}
-}
-
-void WC(char *a)
-{
-	char *b = a;
-	char *left = a;
-	char *right = a + JS(a) - 1;
-	assert(a != NULL);
-	NZ(left,right);
-	while (*b)
-	{
-		char *str = b;
-		while ((*b != ' ') && (*b != '\0'))
-			b++;
-		NZ(str, b-1);
-		if (*b == ' ')
-			b++;
-	}
+	if (n < 10)
+		return n;
+	else
+		return n % 10 + DigitSum(n / 10);
 }
 
 int main()
 {
-	char a[] = "student a am i";
-	WC(a);
-	printf("%s\n", a);
-	getchar();
+	int n = 0;
+	scanf("%d", &n);
+	printf("%d\n", DigitSum(n));
 	return 0;
 }
